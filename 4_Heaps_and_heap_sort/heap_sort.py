54cecf73d1d3125c01019a7d8d9cef09740c0006
Tae Hee Choi <dadac76@hotmail.com>
2023-03-27 23:10:34 -0700

    heap sort

---
+++ 4_Heaps_and_heap_sort/heap_sort.py
@@ -0,0 +1,37 @@
+# Heap Sort has a time complexity of O(n log n). 
+# The efficiency of Heap Sort depends on the time required to build the max-heap, which has a time complexity of O(n), 
+#and the time required to perform the swap and reconstruct the max-heap, which has a time complexity of O(log n). 
+#Therefore, the overall time complexity of Heap Sort is O(n log n).
+# Heap Sort is an in-place sorting algorithm requiring O(1) space complexity.
+
+def heapify(arr, n, i):
+    largest = i
+    l = 2 * i + 1
+    r = 2 * i + 2
+
+    if l < n and arr[i] < arr[l]:
+        largest = l
+
+    if r < n and arr[largest] < arr[r]:
+        largest = r
+
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]
+        heapify(arr, n, largest)
+
+def heapSort(arr):
+    n = len(arr)
+
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(arr, n, i)
+
+    for i in range(n-1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]
+        heapify(arr, i, 0)
+
+arr = [12, 11, 13, 5, 6, 7]
+heapSort(arr)
+n = len(arr)
+print("Sorted array is")
+for i in range(n):
+    print("%d" % arr[i])
